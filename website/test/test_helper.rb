8ce8efff81549079b428ba14fffef6e7b0415a77
Builder106 <vaughanolayinka@gmail.com>
2026-09-08 08:22:10 -0400

    build(deps): reconcile gemfile lock and exclude rake tasks from minitest coverage

--- website/test/test_helper.rb
+++ website/test/test_helper.rb
@@ -3,6 +3,7 @@ require "simplecov"
 SimpleCov.start "rails" do
   enable_coverage :branch
   minimum_coverage line: 100, branch: 100
+  skip "lib/tasks"
 end
 require_relative "../config/environment"
 require "rails/test_help"
